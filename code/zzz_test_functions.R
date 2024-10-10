576066a80934ef2380ce5e4479c896efc6af07b6
timriffe <tim.riffe@gmail.com>
2024-10-10 09:08:28 +0200

    wraps the decomposition in a single function; performed on all education groups. Missing Kitagawa step

--- code/zzz_test_functions.R
+++ code/zzz_test_functions.R
@@ -3,63 +3,78 @@
 
 source("https://raw.githubusercontent.com/timriffe/ms_sensitivity/master/R/00_functions_classic.R")
 source("https://raw.githubusercontent.com/timriffe/ms_sensitivity/master/R/00_sensitivity_functions.R")
-
+library(readxl)
 
 library(tidyverse)
 IN <- read_csv("data/transitions.csv")
+IN2 <- read_excel("data/TP_2016_2020.xlsx") |> 
+  select(1:10)
+IN2$educ |> unique()
 
-IN |> head()
+do_dec <- function(data){
+  prev <- data |> 
+    filter(age == 40)
+  trans <- data |> 
+    filter(age > 40) 
+  
+  init_pars <- 
+    prev |> 
+    select(sex, H = HH, U = UU) |> 
+    pivot_longer(H:U, names_to = "state", values_to = "p") |> 
+    pivot_wider(names_from = sex, values_from = p) |> 
+    mutate(delta = f - m,
+           p = (f + m) / 2)
+  
+  init <- init_pars$p
+  names(init) <- c("H","U")
+  
+  decomp_data <- 
+    trans |> 
+    pivot_longer(HH:UD, names_to = "transition", values_to = "p") |> 
+    pivot_wider(names_from = sex, values_from = p) |> 
+    mutate(delta = f - m,
+           p = (f + m) / 2) |> 
+    select(-m, -f, -Age) |> 
+    arrange(transition, age) |> 
+    mutate(age = age - min(age))
+  
+  
+  sen <-
+    decomp_data |> 
+    s2t(init = init, expectancy = "h", interval = .25) |> 
+    mutate(effect = effect)
+  
+  
+  dec <- 
+    init_pars |> 
+    filter(state == "H") |> 
+    select(-state,-m,-f) |> 
+    mutate(transition = "init", age = 0) |> 
+    bind_rows(decomp_data) |> 
+    filter(! transition %in% c("UU","HH","UH","UD")) |> 
+    left_join(sen, by = join_by("transition", "age")) |> 
+    mutate(cc = delta * effect) |> 
+    mutate(age = age + 40)
+  
+  dec
+}
+dec2 <- 
+IN2 |> 
+  group_by(educ) |> 
+  group_modify(~do_dec(data = .x))
 
-prev <- IN |> 
-  filter(age == 40)
-trans <- IN |> 
-  filter(age > 40) 
-
-init_pars <- 
-  prev |> 
-  filter(educ == "total") |> 
-  select(sex, H = HH, U = UU) |> 
-  pivot_longer(H:U, names_to = "state", values_to = "p") |> 
-  pivot_wider(names_from = sex, values_from = p) |> 
-  mutate(delta = f - m,
-         p = (f + m) / 2)
-
-init <- init_pars$p
-names(init) <- c("H","U")
-
-decomp_data <- 
-  trans |> 
-  filter(educ == "total") |> 
-  pivot_longer(HH:UD, names_to = "transition", values_to = "p") |> 
-  pivot_wider(names_from = sex, values_from = p) |> 
-  mutate(delta = f - m,
-         p = (f + m) / 2) |> 
-  select(-m, -f, -Age) |> 
-  arrange(transition, age) |> 
-  mutate(age = age - min(age))
-
-sen <-
- decomp_data |> 
-  s2t(init = init, expectancy = "h", interval = .25) |> 
-  mutate(effect = effect)
 
 
-dec <- 
-  init_pars |> 
-  filter(state == "H") |> 
-  select(-state,-m,-f) |> 
-  mutate(transition = "init", educ = "total", age = 0) |> 
-  bind_rows(decomp_data) |> 
-  filter(! transition %in% c("UU","HH","UH","UD")) |> 
-  left_join(sen, by = join_by("transition", "age")) |> 
-  mutate(cc = delta * effect) |> 
-  mutate(age = age+ 40)
 
 
-dec |> 
+
+dec2 |> 
+  filter(transition == "init")
+
+dec2 |> 
   filter(transition != "init") |> 
   mutate(age = age) |>
-  ggplot(aes(x = age, y = cc, color = transition)) +
+  ggplot(aes(x = age, y = cc, color = transition, linetype = educ)) +
   geom_line() 
 
 dec$cc |> sum()
