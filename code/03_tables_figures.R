f779c3bea530efaf14da059cde01d6d4a61121e7
timriffe <tim.riffe@gmail.com>
2024-10-12 17:14:39 +0200

    lil code mangement

--- code/03_tables_figures.R
+++ code/03_tables_figures.R
@@ -18,3 +18,27 @@ dec_total |>
   pivot_wider(names_from = educ, values_from = cc) |> 
   pivot_longer(-transition, names_to = "educ", values_to = "cc") |> 
   pivot_wider(names_from = transition, values_from = cc)
+
+# I suppose there is a clever way to compose a table with all this info,
+# plus the Kitagawa parameters.
+
+p1 <- 
+dec_total |> 
+  filter(transition != "init") |> 
+  mutate(age = age) |>
+  ggplot(aes(x = age, y = cc, color = transition, linetype = educ)) +
+  geom_line() +
+  theme_minimal() +
+  labs(title = "Contribution to education-specific gender gap in CVDFLE")
+
+p2 <-
+dec_total |> 
+  filter(transition != "init") |> 
+  mutate(age = age) |>
+  ggplot(aes(x = age, y = cc_total, color = transition, linetype = educ)) +
+  geom_line() +
+  theme_minimal() +
+  labs(title = "Contribution to total gender gap in CVDFLE")
+
+p1
+p2
