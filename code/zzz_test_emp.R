ca2f3090f8f9c84e01cc88d1ff222fbd70078cf6
timriffe <timriffe@gmail.com>
2025-01-29 15:48:48 +0100

    compare fits of naive smooth spline with modelled and empirical transitions. it's a big improvement, but can still improve some

---
+++ code/zzz_test_emp.R
@@ -0,0 +1,73 @@
+
+library(tidyverse)
+library(readxl)
+
+file_names <- c("TP_2000_04.xlsx",
+                "TP_2016_20.xlsx")
+dat_out <- structure(list(period = character(0), gender = character(0), 
+                          educ = character(0), age = numeric(0), transition = character(0), 
+                          state_from = character(0), EMP = numeric(0), MOD = numeric(0), 
+                          denom = numeric(0)), class = c("tbl_df", "tbl", "data.frame"
+                          ), row.names = integer(0))
+for (f in file_names){
+  
+  period = substr(f,4,10)
+  for (s in excel_sheets(file.path("data",f))){
+    name_parts <- str_split(s, pattern = " ")[[1]]
+    educ   <- name_parts[1]
+    educ   <- ifelse(educ == "Overall","Total", educ)
+    gender <- name_parts[2]
+    ini    <- read_excel(file.path("data",f), sheet = s) |> 
+      rename(age = 1) |> 
+      select(!contains("...")) |> 
+      mutate(age = (age - (age %% 12))/12 + ( (age %% 12) / 12)) |> 
+      pivot_longer(-age, names_to = c("variant","transition"), names_sep = " ", values_to = "p") |> 
+      mutate(period = period, gender = gender, educ = educ,.before = 1) |> 
+      mutate(state_from = substr(transition,1,1))
+    
+    denom <- ini |> 
+      filter(variant == "DENOM") |> 
+      select(denom = p,state_from, age)
+    
+    ini2 <- 
+      ini |> 
+      filter(variant != "DENOM") |> 
+      pivot_wider(names_from = variant, values_from = p) |> 
+      left_join(denom, by = join_by(state_from, age))
+    
+    dat_out <- bind_rows(dat_out, ini2)
+  }
+}
+
+dat_out <- 
+  dat_out |> 
+  mutate(gender = tolower(gender),
+         educ = tolower(educ)) |> 
+  filter(!is.na(age))
+
+
+smooth.spline.chunk <- function(chunk, 
+                                age_out =  seq(40,99.75,by=.25)){
+  chunk <- chunk |> filter(!is.na(EMP))
+  mod <- smooth.spline(x = chunk$age, y = log(chunk$EMP), w = sqrt(chunk$denom), df= 5)
+  tibble(age = age_out,
+         sm_sp = predict(mod, x = age_out)$y |> exp())
+}
+sm_compare <- 
+dat_out |> 
+  group_by(period, gender, educ, transition) |> 
+  group_modify(~ smooth.spline.chunk(chunk = .x)) |> 
+  left_join(dat_out, by = join_by(period, gender, educ, transition,age))
+
+sm_compare |> 
+  filter(educ == "total",     #"tertiary" "basic" "secondary" "total"
+         gender == "women") |>   # "men" "women"
+  ggplot(aes(x = age, y = sm_sp)) +
+  geom_line(linewidth = 2) +
+  scale_y_log10() +
+  geom_point(mapping = aes(y = EMP), alpha = .25) +
+  geom_line(mapping = aes(y = MOD), color = "red") +
+  facet_wrap(period~transition)
+  
+
+  
