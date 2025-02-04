8a5d1ac0772691ea545c8684dd6168b3a1440c73
timriffe <tim.riffe@gmail.com>
2025-02-04 22:16:07 +0100

    time decomp is now refactored for the new data

--- code/03_decompose_time.R
+++ code/03_decompose_time.R
@@ -1,38 +1,48 @@
 source("code/00_setup.R")
 source("code/01_functions.R")
-IN1 <- read_excel("data/TP_2000_2004.xlsx") |> 
-  fselect(1:10)
-IN2 <- read_excel("data/TP_2016_2020.xlsx") |> 
-  fselect(1:10)
 
-IN1 <-
-  IN1 |> 
-  mutate(period = "2000-2004",
-         time_mid = 2002)
-IN2 <-
-  IN2 |> 
-  mutate(period = "2016-2020",
-         time_mid = 2018)
-
-IN <-
-  bind_rows(IN1, IN2)
-
-dec <-
-  IN |> 
-  group_by(educ, sex)
-
+# IN1 <- read_excel("data/TP_2000_2004.xlsx") |> 
+#   fselect(1:10)
+# IN2 <- read_excel("data/TP_2016_2020.xlsx") |> 
+#   fselect(1:10)
+# 
+# IN1 <-
+#   IN1 |> 
+#   mutate(period = "2000-2004",
+#          time_mid = 2002)
+# IN2 <-
+#   IN2 |> 
+#   mutate(period = "2016-2020",
+#          time_mid = 2018)
+# 
+# IN <-
+#   bind_rows(IN1, IN2)
+# 
+# head(IN)
+IN <- read_csv("data/TP_final.csv.gz", show_col_types = FALSE)  |> 
+  filter(version == "p_ps") |> 
+  select(period, gender, educ, transition, age, p) |> 
+  pivot_wider(names_from = transition, values_from = p) |> 
+  mutate(HH = if_else(is.na(HH),1 - HU - HD, HH),
+         UH = 0,
+         UU = if_else(is.na(UU), 1 - UD - UH, UU),
+         time_mid = if_else(period == "2016-2020",2018,2002)) |> 
+  arrange(period, educ, gender, age)
+IN |> head()
 # this is 8 different decompositions, so it takes
 # slightly longer than before
-dec <- IN |> 
-  group_by(educ, sex) |> 
+dec <-
+  IN |> 
+  group_by(educ, gender) |> 
   group_modify(~do_dec_time(data = .x)) |> 
   ungroup()
+
 
 # To know how to weight these, we need all cvd-free life
 # expectancies, for use in a Kitagawa decomposition.
 expectancies <-
   IN |> 
-  group_by(educ, sex, period) |> 
+  group_by(educ, gender, period) |> 
   summarize(HLE = calc_expectancy(ptibble=.data,expectancy = "h"),
             .groups = "drop") |> 
   pivot_wider(names_from = period, values_from = HLE, names_prefix = "HLE_")
@@ -49,7 +59,7 @@ total_non_stationary <-
 # Only need educ-specific expectancies for kitagawa
 le_for_kit <- expectancies |> 
   filter(educ != "total") |> 
-  arrange(sex, educ) |> 
+  arrange(gender, educ) |> 
   rename(HLE_1 = `HLE_2000-2004`,
          HLE_2 = `HLE_2016-2020`)
 
@@ -64,12 +74,12 @@ le_for_kit <- expectancies |>
 
 # Prevalence values hard coded. These come from exact age 40; we may replace
 # these prevalence averages from ages 38-42 or so. 
-prev_for_kit <- tibble(educ = c("basic","intermediate","tertiary",
-                                "basic","intermediate","tertiary"),
-                       sex = c(rep("m",3),rep("f",3)),
+prev_for_kit <- tibble(educ = c("basic","secondary","tertiary",
+                                "basic","secondary","tertiary"),
+                       gender = c(rep("men",3),rep("women",3)),
                        prev_1 = c(0.1685,0.5057,0.3258,0.0961,0.4350,0.4689),
                        prev_2 = c(0.1554,0.4693,0.3753,0.0787,0.3544,0.5669)) |> 
-  group_by(sex) |> 
+  group_by(gender) |> 
   mutate(prev_1 = prev_1 / sum(prev_1),
          prev_2 = prev_2 / sum(prev_2))
 
@@ -77,7 +87,7 @@ prev_for_kit <- tibble(educ = c("basic","intermediate","tertiary",
 # rate effects we use to reweight group0-wise decompositions. This is two decomps,
 # but no need for group_by() here.
 kit <- 
-  left_join(le_for_kit, prev_for_kit, by = join_by(sex,educ)) |> 
+  left_join(le_for_kit, prev_for_kit, by = join_by(gender,educ)) |> 
   mutate(HLE_d = HLE_2 - HLE_1,
          HLE_avg = (HLE_2 + HLE_1) / 2,
          prev_avg = (prev_2 + prev_1) / 2,
@@ -88,14 +98,14 @@ kit <-
 # This is used to weight educ-wide decompositions...
 dec_weights <-
   kit |> 
-  select(sex, educ, rate_effect)
+  select(gender, educ, rate_effect)
 
 
 # produce stationary total expectancy, blending edu according
 # to radix composition.
 total_stationary <-
   kit |> 
-  group_by(sex) |> 
+  group_by(gender) |> 
   summarize(educ = "total",
             `HLE_2000-2004` = sum(HLE_1 * prev_1),
             `HLE_2016-2020` = sum(HLE_2 * prev_2)) |> 
@@ -112,20 +122,20 @@ total_hle <-
 dec_total <-
   dec |> 
   filter(educ != "total") |> 
-  left_join(dec_weights, by = join_by(educ,sex)) |> 
-  group_by(sex,educ) |> 
+  left_join(dec_weights, by = join_by(educ,gender)) |> 
+  group_by(gender,educ) |> 
   mutate(cc_total = (cc / sum(cc)) * rate_effect)
 
 # Our complete decomposition, to check sums
 dt <- 
   dec_total |> 
-  group_by(sex) |> 
+  group_by(gender) |> 
   summarize(cc_rates = sum(cc_total))
 
 kit |> 
-  group_by(sex) |> 
+  group_by(gender) |> 
   summarize(cc_structure = sum(structure_effect)) |> 
-  left_join(dt, by = join_by(sex)) |> 
+  left_join(dt, by = join_by(gender)) |> 
   mutate(Delta = cc_rates + cc_structure)
 # check above
 # 1.7 increase in CVDFLE for females, 2.35 for males
