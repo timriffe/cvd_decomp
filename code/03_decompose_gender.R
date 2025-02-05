43a0ceda1843be86e59a58f15d4f65b31e29b3bb
timriffe <timriffe@gmail.com>
2025-02-05 10:25:01 +0100

    03_decompose_gender.R now refactored for new data; results produced for both periods

--- code/03_decompose_gender.R
+++ code/03_decompose_gender.R
@@ -1,24 +1,36 @@
 source("code/00_setup.R")
 source("code/01_functions.R")
 
-IN2 <- read_excel("data/TP_2016_2020.xlsx") |> 
-  fselect(1:10)
+# IN2 <- read_excel("data/TP_2016_2020.xlsx") |> 
+#   fselect(1:10)
+
+IN <- read_csv("data/TP_final.csv.gz",show_col_types = FALSE) |> 
+  filter(version == "p_ps") |> 
+  select(period, gender, educ, transition, age, p) |> 
+  pivot_wider(names_from = transition, values_from = p) |> 
+  mutate(HH = if_else(is.na(HH),1 - HU - HD, HH),
+         UH = 0,
+         UU = if_else(is.na(UU), 1 - UD - UH, UU),
+         time_mid = if_else(period == "2016-2020",2018,2002)) |> 
+  arrange(period, educ, gender, age)
+
+prev_edu <- read_csv("data/prev_edu.csv",show_col_types = FALSE)
 
 # sex decompositions between like-education groups, i.e.
 # basic vs basic, etc includes initial health conditions.
-dec2 <- 
-  IN2 |> 
-  group_by(educ) |> 
+dec <- 
+  IN |> 
+  group_by(educ, period) |> 
   group_modify(~do_dec_gender(data = .x)) |> 
   ungroup()
 
 # To know how to weight these, we need all cvd-free life
 # expectancies, for use in a Kitagawa decomposition.
 expectancies <-
-  IN2 |> 
-  group_by(educ, sex) |> 
-  summarize(HLE = calc_expectancy(ptibble=.data)) |> 
-  pivot_wider(names_from = sex, values_from = HLE, names_prefix = "HLE_")
+  IN |> 
+  group_by(educ, gender, period) |> 
+  summarize(HLE = calc_expectancy(ptibble=.data),.groups = "drop") |> 
+  pivot_wider(names_from = gender, values_from = HLE, names_prefix = "HLE_")
 
 # An expectancy calculated based on the total education group
 # is not stationary insofar as the underlying education composition
@@ -34,35 +46,42 @@ le_for_kit <- expectancies |>
 
 # Prevalence values hard coded. These come from exact age 40; we may replace
 # these prevalence averages from ages 38-42 or so. 
-prev_for_kit <- tibble(educ = c("basic","intermediate","tertiary"),
-                       prev_m = c(.155,.469,.375),
-                       prev_f = c(.079,.354,.567)) |> 
-  mutate(prev_m = prev_m / sum(prev_m),
-         prev_f = prev_f / sum(prev_f))
+# prev_for_kit <- tibble(educ = c("basic","secondary","tertiary"),
+#                        prev_m = c(.155,.469,.375),
+#                        prev_f = c(.079,.354,.567)) |> 
+#   mutate(prev_m = prev_m / sum(prev_m),
+#          prev_f = prev_f / sum(prev_f))
 
+prev_edu |> group_by(period, gender) |> summarize(check = sum(init))
+
+prev_for_kit <-
+  prev_edu |> 
+  rename(prev = init) |> 
+  pivot_wider(names_from = gender, values_from = prev, names_prefix = "prev_")
 # Perform Kitagawa decomposition. Structure effects must be summed;
 # rate effects we use to reweight group0-wise decompositions.
-kit <- left_join(le_for_kit, prev_for_kit, by = join_by(educ)) |> 
-  mutate(HLE_d = HLE_f - HLE_m,
-         HLE_avg = (HLE_f + HLE_m) / 2,
-         prev_avg = (prev_f + prev_m) / 2,
-         prev_d = prev_f - prev_m,
+kit <- left_join(le_for_kit, prev_for_kit, by = join_by(educ, period)) |> 
+  mutate(HLE_d = HLE_women - HLE_men,
+         HLE_avg = (HLE_women + HLE_men) / 2,
+         prev_avg = (prev_women + prev_men) / 2,
+         prev_d = prev_women - prev_men,
          structure_effect = prev_d * HLE_avg,
          rate_effect = HLE_d * prev_avg)
 
 # This is used to weight educ-wide decompositions...
 dec_weights <-
   kit |> 
-  select(educ, rate_effect)
+  select(period, educ, rate_effect)
 
 # produce stationary total expectancy, blending edu according
 # to radix composition.
 total_stationary <-
   kit |> 
   ungroup() |> 
+  group_by(period) |> 
   summarize(educ = "total",
-            HLE_f = sum(HLE_f * prev_f),
-            HLE_m = sum(HLE_m * prev_m)) |> 
+            HLE_f = sum(HLE_women * prev_women),
+            HLE_m = sum(HLE_men * prev_men)) |> 
   mutate(variant = "stationary", .before = 1)
 
 
