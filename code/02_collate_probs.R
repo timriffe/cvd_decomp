374952492aca9e16841f99450e1077effd21ea34
timriffe <tim.riffe@gmail.com>
2025-02-13 10:18:01 +0100

    create denominators file

--- code/02_collate_probs.R
+++ code/02_collate_probs.R
@@ -53,3 +53,57 @@ emp_probs <-
 # save
 write_csv(emp_probs,"data/emp_probs_cod.csv.gz")
 
+# repeat for files without COD because these have denominators!
+if (run_this){
+  file_names <- c("TP_2000_04.xlsx",
+                  "TP_2016_20.xlsx")
+  dat_out <- structure(list(period = character(0), gender = character(0), 
+                            educ = character(0), age = numeric(0), transition = character(0), 
+                            state_from = character(0), EMP = numeric(0), MOD = numeric(0), 
+                            denom = numeric(0)), class = c("tbl_df", "tbl", "data.frame"
+                            ), row.names = integer(0))
+  for (f in file_names){
+    
+    period = substr(f,4,10)
+    for (s in excel_sheets(file.path("data",f))){
+      name_parts <- str_split(s, pattern = " ")[[1]]
+      educ   <- name_parts[1]
+      educ   <- ifelse(educ == "Overall","Total", educ)
+      gender <- name_parts[2]
+      ini    <- read_excel(file.path("data",f), sheet = s) |> 
+        rename(age = 1) |> 
+        select(!contains("...")) |> 
+        mutate(age = (age - (age %% 12))/12 + ( (age %% 12) / 12)) |> 
+        pivot_longer(-age, names_to = c("variant","transition"), names_sep = " ", values_to = "p") |> 
+        mutate(period = period, gender = gender, educ = educ,.before = 1) |> 
+        mutate(state_from = substr(transition,1,1))
+      
+      denom <- ini |> 
+        filter(variant == "DENOM") |> 
+        select(denom = p,state_from, age)
+      
+      ini2 <- 
+        ini |> 
+        filter(variant != "DENOM") |> 
+        pivot_wider(names_from = variant, values_from = p) |> 
+        left_join(denom, by = join_by(state_from, age))
+      
+      dat_out <- bind_rows(dat_out, ini2)
+    }
+  }
+  
+  dat_out <- 
+    dat_out |> 
+    mutate(gender = tolower(gender),
+           educ = tolower(educ)) |> 
+    filter(!is.na(age))
+  
+  write_csv(dat_out, file = "data/TP_emp.csv.gz")
+}
+emp <- read_csv("data/TP_emp.csv.gz")
+denoms <- 
+  emp |> 
+  mutate(period = if_else(period == "2000_04","2000-2004","2016-2020")) |> 
+  group_by(period, gender, educ, age, state_from) |> 
+  summarize(denom = denom[1], .groups = "drop")
+write_csv(denoms, "data/denoms.csv.gz")
